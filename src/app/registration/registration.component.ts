85a8f385f2a551ffd3960843708044c5237a8a46
bohdan <myketenko.bohdan@gmail.com>
2018-07-20 12:05:19 +0300

    Used form builder for registration form.

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -10,42 +10,18 @@ export class RegistrationComponent implements OnInit {
     public registrationForm: FormGroup;
     public formTouched: boolean = false;
 
-    constructor(private formBuilder: FormBuilder) {
+    constructor(private fb: FormBuilder) {
     }
 
     ngOnInit() {
-        this.registrationForm = this.formBuilder.group({
-            userName: new FormControl('', [
-                Validators.required,
-                Validators.minLength(3)
-            ]),
-            email: new FormControl('', [
-                Validators.required,
-                Validators.email
-            ]),
-            password: new FormControl('', [
-                Validators.required,
-                Validators.minLength(3)
-            ]),
-            confirmPassword: new FormControl('', [
-                Validators.required,
-                Validators.minLength(3),
-                this.passwordsAreEqual()
-            ]),
+        this.registrationForm = this.fb.group({
+            userName: ['', [Validators.required, Validators.minLength(3)]],
+            email: ['', [Validators.required, Validators.email]],
+            password: this.fb.group({
+                pwd: ['', [Validators.required, Validators.minLength(3)]],
+                confirm: ['', [Validators.required, Validators.minLength(3)]]
+            }, {validator: RegistrationComponent.passwordsAreEqual()})
         });
-    }
-
-    // TODO: Fixed passwords validator; Add confirm password and password in one FormGroup;
-    private passwordsAreEqual(): ValidatorFn {
-        return (group: FormGroup) => {
-            console.log(group);
-
-            if (!(group.dirty || group.touched) || group.get('password').value === group.get('confirmPassword').value) {
-                return null;
-            }
-
-            return {passwordEqual: true}
-        };
     }
 
     send() {
@@ -53,5 +29,15 @@ export class RegistrationComponent implements OnInit {
 
         console.log(this.registrationForm.controls);
         // console.log(this.registrationForm.value);
+    }
+
+    private static passwordsAreEqual(): ValidatorFn {
+        return (group: FormGroup) => {
+            if (!(group.dirty || group.touched) || group.get('pwd').value === group.get('confirm').value) {
+                return null;
+            }
+
+            return {passwordEqual: true}
+        };
     }
 }
