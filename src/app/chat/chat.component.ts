6b20c3b92f6b7a2b4fed40b2f0d6aea7d44724cd
bohdan <myketenko.bohdan@gmail.com>
2018-07-26 13:01:41 +0300

    Added auto scroll to bottom for messages.

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, AfterContentInit, OnDestroy} from '@angular/core';
+import {Component, OnInit, OnDestroy, OnChanges, ElementRef, ViewChild, AfterViewChecked} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import {ChatService} from './chat.service';
 import {Message} from './chat.model';
@@ -8,22 +8,24 @@ import {Message} from './chat.model';
     templateUrl: './chat.component.html',
     styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit, OnDestroy {
+export class ChatComponent implements OnInit, OnDestroy, AfterViewChecked {
     // Array<Message> === Message[]
-    // AfterContentInit - module for init after load content
+    @ViewChild('scrollBottom') private scrollContainer: ElementRef;
     public messageForm: FormGroup = new FormGroup({
         messageControl: new FormControl('')
     });
-    // public messageControl: FormControl = new FormControl('');
     public messages: Array<Message> = [];
-    private username = '';
     public userId = '';
+    private username = '';
+    private disableScrollDown = false;
     private timer: any;
 
     constructor(private chatService: ChatService) {
     }
 
     ngOnInit() {
+        this.disableScrollDown = false;
+
         this.getChats();
 
         if (localStorage.getItem('user')) {
@@ -33,9 +35,9 @@ export class ChatComponent implements OnInit, OnDestroy {
             this.userId = currentUser['userId'];
         }
 
-        // remove after adding sokets.
+        // remove after adding sockets.
         this.timer = setInterval(() => {
-            // this.getChats();
+            this.getChats();
         }, 5000)
     }
 
@@ -43,10 +45,15 @@ export class ChatComponent implements OnInit, OnDestroy {
         clearInterval(this.timer);
     }
 
+    ngAfterViewChecked() {
+        this.scrollToBottom();
+    }
+
     private getChats() {
         this.chatService.getMessages()
             .subscribe((data: Message[]) => {
                 this.messages = data;
+
                 console.log(this.messages);
             }, err => {
                 console.log(err);
@@ -67,8 +74,26 @@ export class ChatComponent implements OnInit, OnDestroy {
                 this.messageForm.reset();
                 // update chat
                 this.getChats();
+
+                this.disableScrollDown = false;
             }, (err) => {
                 console.error(err);
             });
+    }
+
+    public onScroll() {
+        this.disableScrollDown = true;
+    }
+
+    private scrollToBottom(): boolean {
+        if (this.disableScrollDown) {
+            return false;
+        }
+
+        try {
+            this.scrollContainer.nativeElement.scrollTop = this.scrollContainer.nativeElement.scrollHeight;
+        } catch (err) {
+            console.log(err);
+        }
     }
 }
