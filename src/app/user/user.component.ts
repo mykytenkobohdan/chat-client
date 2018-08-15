d879bde5b23a72c165c80cfa786ffeea54ecec81
= <myketenko.bohdan@gmail.com>
2018-08-15 17:58:51 +0300

    updateOn: 'blur'

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -76,6 +76,9 @@ export class UserComponent implements OnInit {
     this.userForm = new FormGroup({
       username: new FormControl(this.user.username, [Validators.required, Validators.minLength(3)]),
       email: new FormControl(this.user.email, [Validators.required, Validators.email]),
+      // dateFormatted: new FormControl(this.forecast.dateFormatted, 
+      // { validators: Validators.required, asyncValidators: 
+      // [this.forecastValidators.existingDateValidator(this.forecast.dateFormatted)], updateOn: 'blur' }),
       // updateOn: 'blur'
       currentPassword: new FormControl('', [Validators.required, this.validateCurPassword(this.oldUser.password)])
     });
