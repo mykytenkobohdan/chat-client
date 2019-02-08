1545e85c209c81bd3d08a926b192d508febb193e
mykytenkobohdan <mykytenko.bohdan@gmail.com>
2019-02-08 20:01:19 +0200

    Create PWA. Fixed conflict between socet.io and service worker.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { AppGuard } from './app.guard';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {AppGuard} from './app.guard';
 
 const routes: Routes = [
-    // { path: '', redirectTo: 'chat', pathMatch: 'full' },
-    {
-        path: '',
-        canActivate: [AppGuard],
-        // lazy load module
-        loadChildren: './chat/chat.module#ChatModule'
-    },
-    { path: 'registration', loadChildren: './registration/registration.module#RegistrationModule' },
-    { path: 'auth', loadChildren: './auth/auth.module#AuthModule' },
-    {
-        path: 'user/:userId',
-        canActivate: [AppGuard],
-        loadChildren: './user/user.module#UserModule'
-    },
-    { path: '**', redirectTo: '' }
+  {
+    path: '',
+    // canActivate: [AppGuard],
+    loadChildren: 'src/app//chat/chat.module#ChatModule'
+  },
+  {path: 'registration', loadChildren: 'src/app/registration/registration.module#RegistrationModule'},
+  {path: 'auth', loadChildren: 'src/app/auth/auth.module#AuthModule'},
+  {
+    path: 'user/:userId',
+    canActivate: [AppGuard],
+    loadChildren: 'src/app/user/user.module#UserModule'
+  },
+  {path: '**', redirectTo: ''}
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {
 }
--- src/app/web-socket.service.ts
+++ src/app/web-socket.service.ts
@@ -1,77 +1,79 @@
-import { Injectable } from '@angular/core';
+import {Injectable, NgZone} from '@angular/core';
 import * as io from 'socket.io-client';
-import { Observable } from 'rxjs';
-import { environment } from '../environments/environment';
-import { Message } from './chat/chat.model';
+import {Observable} from 'rxjs';
+import {environment} from '../environments/environment';
+import {Message} from './chat/chat.model';
 
 export enum Event {
-    CONNECT = 'connect',
-    DISCONNECT = 'disconnect'
+  CONNECT = 'connect',
+  DISCONNECT = 'disconnect'
 }
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class WebSocketService {
-    private socket;
-    private host = environment.host;
+  private socket;
+  private host = environment.host;
 
-    constructor() {
-    }
+  constructor(private ngZone: NgZone) {
+  }
 
-    public initSocket(): void {
-        this.socket = io(this.host);
-    }
+  public initSocket(): void {
+    this.ngZone.runOutsideAngular(() => {
+      this.socket = io(this.host);
+    });
+  }
 
-    public send(message: Message): void {
-        this.socket.emit('message', message);
-    }
+  public send(message: Message): void {
+    this.socket.emit('message', message);
+  }
 
-    public update(message: Message): void {
-        this.socket.emit('update-message', message);
-    }
+  public update(message: Message): void {
+    this.socket.emit('update-message', message);
+  }
 
-    public onCreateMessage(): Observable<any> {
-        return new Observable<any>(observer => {
-            this.socket.on('message', (data) => {
-                observer.next(data);
-            });
+  public onCreateMessage(): Observable<any> {
+    return new Observable<any>(observer => {
+      this.socket.on('message', (data) => {
+        observer.next(data);
+      });
 
-            return () => {
-                this.socket.disconnect();
-            };
-        });
-    }
+      return () => {
+        this.socket.disconnect();
+      };
+    });
+  }
 
-    public onUpdateMessage(): Observable<any> {
-        return new Observable<any>(observer => {
-            this.socket.on('update-message', (data) => {
-                observer.next(data);
-            });
+  public onUpdateMessage(): Observable<any> {
+    return new Observable<any>(observer => {
+      this.socket.on('update-message', (data) => {
+        observer.next(data);
+      });
 
-            return () => {
-                this.socket.disconnect();
-            };
-        });
-    }
+      return () => {
+        this.socket.disconnect();
+      };
+    });
+  }
 
-    public onRemoveMessage(): Observable<any> {
-        return new Observable<any>(observer => {
-            this.socket.on('remove-message', (data) => {
-                observer.next(data);
-            });
+  public onRemoveMessage(): Observable<any> {
+    return new Observable<any>(observer => {
+      this.socket.on('remove-message', (data) => {
+        observer.next(data);
+      });
 
-            return () => {
-                this.socket.disconnect();
-            };
-        });
-    }
+      return () => {
+        this.socket.disconnect();
+      };
+    });
+  }
 
-    public onEvent(event: Event): Observable<any> {
-        return new Observable<Event>(observer => {
-            console.log('socket', event);
-            this.socket.on(event, () => observer.next());
-        });
-    }
+  public onEvent(event: Event): Observable<any> {
+    return new Observable<Event>(observer => {
+      console.log('socket', event);
+      this.socket.on(event, () => observer.next());
+    });
+  }
 }
 
